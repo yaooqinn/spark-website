39e82c393a0e31d9915fd252247ae273e9471925
yaooqinn <yaooqinn@users.noreply.github.com>
2024-01-26 05:32:36 +0000

    deploy: 185d1e3ad285f0b9ad7ba00a4dfbb1a4b1458d35

--- site/js/downloads.js
+++ site/js/downloads.js
@@ -13,18 +13,24 @@ function addRelease(version, releaseDate, packages, mirrored) {
 
 var sources = {pretty: "Source Code", tag: "sources"};
 var hadoopFree = {pretty: "Pre-built with user-provided Apache Hadoop", tag: "without-hadoop"};
-var hadoop2p6 = {pretty: "Pre-built for Apache Hadoop 2.6", tag: "hadoop2.6"};
 var hadoop2p7 = {pretty: "Pre-built for Apache Hadoop 2.7", tag: "hadoop2.7"};
-var hadoop3p2 = {pretty: "Pre-built for Apache Hadoop 3.2 and later", tag: "hadoop3.2"};
-var scala2p12_hadoopFree = {pretty: "Pre-built with Scala 2.12 and user-provided Apache Hadoop", tag: "without-hadoop-scala-2.12"};
+var hadoop3p3 = {pretty: "Pre-built for Apache Hadoop 3.2 and later", tag: "hadoop3.2"};
+var hadoop3p3scala213 = {pretty: "Pre-built for Apache Hadoop 3.2 and later (Scala 2.13)", tag: "hadoop3.2-scala2.13"};
+var hadoop2p = {pretty: "Pre-built for Apache Hadoop 2.7", tag: "hadoop2"};
+var hadoop3p = {pretty: "Pre-built for Apache Hadoop 3.3 and later", tag: "hadoop3"};
+var hadoop3pscala213 = {pretty: "Pre-built for Apache Hadoop 3.3 and later (Scala 2.13)", tag: "hadoop3-scala2.13"};
 
-// 2.4.0+
-var packagesV9 = [hadoop2p7, hadoop2p6, hadoopFree, scala2p12_hadoopFree, sources];
-// 3.0.0+
-var packagesV10 = [hadoop2p7, hadoop3p2, hadoopFree, sources];
+// 3.2.0+
+var packagesV12 = [hadoop3p3, hadoop3p3scala213, hadoop2p7, hadoopFree, sources];
+// 3.3.0+
+var packagesV13 = [hadoop3p, hadoop3pscala213, hadoop2p, hadoopFree, sources];
+// 3.4.0+
+var packagesV14 = [hadoop3p, hadoop3pscala213, hadoopFree, sources];
 
-addRelease("3.0.1", new Date("09/02/2020"), packagesV10, true);
-addRelease("2.4.6", new Date("06/05/2020"), packagesV9, true);
+addRelease("3.5.0", new Date("09/13/2023"), packagesV14, true);
+addRelease("3.4.1", new Date("06/23/2023"), packagesV14, true);
+addRelease("3.3.3", new Date("08/21/2023"), packagesV13, true);
+addRelease("3.2.4", new Date("04/13/2023"), packagesV12, true);
 
 function append(el, contents) {
   el.innerHTML += contents;
